613172762e21e24bcad6a3ede3065c18735543e7
Rostyslav-Baryshok <85187900+Rostyslav-Baryshok@users.noreply.github.com>
2022-06-09 21:47:58 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const reviewsSwiper = document.querySelector(".swiper-reviews");
 if (reviewsSwiper) {
   const swiper = new Swiper(".swiper-reviews", {
     // Optional parameters
+    autoHeight: true,
     loop: true,
     // If we need pagination
     pagination: {
